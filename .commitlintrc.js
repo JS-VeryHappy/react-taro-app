fd4af851594bc9fc3a716e95af19f213eed5c071
zhouyingchao <1212>
2021-12-21 11:27:08 +0800

    ✨ feat: 2

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
   /* type(scope?): subject  例子：feat(server): test */
   extends: ['@commitlint/config-conventional'], // 使用预设的配置 https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*\w*): (.*)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
   rules: {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', '🐛 fix', '💥feat'],
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', '🐛 fix', '✨ feat'],
     ], // 改变预设中的提交类型
-    // 'type-case': [2, 'always', 'lower-case'], // 提交类型必须使用小写
+    'type-case': [2, 'always', 'lower-case'], // 提交类型必须使用小写
     'type-empty': [2, 'never'], // type不能为空
     // 'header-max-length': [2, 'always', 5], // header内容的最大长度为5
     'subject-min-length': [2, 'always', 1], // subject内容的最小长度为1
