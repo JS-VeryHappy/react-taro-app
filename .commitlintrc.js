b0fbd828e6e98a1b207401bd0712658ab04d85fb
zhouyingchao <1212>
2021-12-21 12:30:10 +0800

    feat(测试): 哈哈

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,17 +1,12 @@
 module.exports = {
   /* type(scope?): subject  例子：feat(server): test */
   extends: ['@commitlint/config-conventional'], // 使用预设的配置 https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(.*\w*): (.*)$/,
-      headerCorrespondence: ['type'],
-    },
-  },
+
   rules: {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', '🐛 fix', '✨ feat'],
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
     ], // 改变预设中的提交类型
     'type-case': [2, 'always', 'lower-case'], // 提交类型必须使用小写
     'type-empty': [2, 'never'], // type不能为空
